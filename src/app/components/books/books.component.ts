aef46fc7029dab01ea5d1dde65cfd4a446493d6d
IgorGabriel23 <igoorgns@gmail.com>
2024-04-29 17:26:41 -0300

    Diretiva ngFor

---
+++ src/app/components/books/books.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Book } from '../../book';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrl: './books.component.css'
+})
+export class BooksComponent {
+  books: Book[] = [
+    {
+      id: 1,
+      title: "Java 24 horas",
+      author: "Glauco Todesco",
+      price: 40.50
+    },
+    {
+      id: 2,
+      title: "Angular 24 horas",
+      author: "Fernanda Maria",
+      price: 36.50
+    },
+    {
+      id: 3,
+      title: "AWS 24 horas",
+      author: "Juliana Silva",
+      price: 43.25
+    },
+    {
+      id: 4,
+      title: "JavaScript 24 horas",
+      author: "Antenor Lucas",
+      price: 28.50
+    }
+  ]
+}
